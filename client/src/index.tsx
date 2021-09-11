f44181595afd27cede10efde7ebd080c77cb8b7a
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-12 00:28:13 +0300

    send token in authorization header

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -3,14 +3,29 @@ import ReactDOM from "react-dom";
 import App from "./App";
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-import {ApolloClient, ApolloProvider, InMemoryCache} from "@apollo/client";
+import {ApolloClient, ApolloProvider, createHttpLink, InMemoryCache} from "@apollo/client";
 import {Provider} from "react-redux";
 import {BrowserRouter} from "react-router-dom";
 import {store} from "./store/store";
+import {setContext} from "@apollo/client/link/context";
+
+const httpLink = createHttpLink({
+    uri: 'http://localhost:3035/query',
+});
+
+const authLink = setContext((_, { headers }) => {
+    const token = localStorage.getItem('fake_store_token');
+    return {
+        headers: {
+            ...headers,
+            authorization: token ? `Bearer ${token}` : "",
+        }
+    }
+});
 
 const client = new ApolloClient({
-    uri: 'http://localhost:3035/query',
     cache: new InMemoryCache(),
+    link: authLink.concat(httpLink),
 });
 
 ReactDOM.render(
