6bcc6cbd150a9431419f42cdca2f0db0ccdff215
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-12 00:50:07 +0300

    pretty cart number

--- client/src/components/nav-bar/Links.tsx
+++ client/src/components/nav-bar/Links.tsx
@@ -6,15 +6,26 @@ const Links: React.FC = () => {
     return (
         <ul className="navbar-nav me-auto mb-2 mb-md-0">
             <li className="nav-item">
-                <Link to="/products" className="nav-link">Products</Link>
+                <Link to="/products" className="nav-link">
+                    <button type="button" className="btn btn-sm btn-close-white position-relative">Products</button>
+                </Link>
             </li>
 
             <li className="nav-item">
-                <Link to="/orders" className="nav-link">Orders</Link>
+                <Link to="/orders" className="nav-link">
+                    <button type="button" className="btn btn-sm btn-success position-relative">Orders</button>
+                </Link>
             </li>
 
             <li className="nav-item">
-                <Link to="/cart" className="nav-link">Cart</Link>
+                <Link to="/cart" className="nav-link">
+                    <button type="button" className="btn btn-sm btn-outline-info position-relative">
+                        Cart
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">5
+                            <span className="visually-hidden">unread messages</span>
+                        </span>
+                    </button>
+                </Link>
             </li>
         </ul>
     )
