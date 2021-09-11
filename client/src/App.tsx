42d021870aa9aed076b4024ff024fd8d8a1033a8
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-11 18:18:19 +0300

    default root is products list

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -23,7 +23,7 @@ const App: React.FC = () => {
                     </Route>
 
                     <Route path="/">
-                        Home
+                        <ProductsList />
                     </Route>
                 </Switch>
             </div>
