4b5e479822e342b874803c22034aa5e64624c5e3
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-11 17:49:29 +0300

    created products service

---
+++ products/cmd/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"github.com/yigitsadic/fake_store/products/product_grpc/product_grpc"
+	"google.golang.org/grpc"
+	"log"
+	"net"
+)
+
+func main() {
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", 9000))
+	if err != nil {
+		log.Fatalf("failed to listen: %v\n", err)
+	}
+
+	grpcServer := grpc.NewServer()
+	s := server{}
+
+	product_grpc.RegisterProductServiceServer(grpcServer, &s)
+
+	log.Println("Started to serve product grpc")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve due to %s\n", err)
+	}
+}
