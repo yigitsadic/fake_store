38aeed5f1fa73079e85817a2e88e4d02263a6fd7
Yiğit Sadıç <yigitsadic@gmail.com>
2021-09-15 01:48:26 +0300

    removed unused import

--- client/src/components/products/ProductDetail.tsx
+++ client/src/components/products/ProductDetail.tsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from "react";
+import React, {useState} from "react";
 import {useAppSelector} from "../../store/hooks";
 import {selectedCurrentUser} from "../../store/auth/auth";
 import {useAddItemToCartMutation} from "../../generated/graphql";
